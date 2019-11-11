3debf0e6d2152a41421e17cfd0f575e26b6d695b
Amit Thapa <amithapa1994@gmail.com>
2019-11-11 22:04:47 +0530

    Async example & http package install in pubspec.

--- world_time/lib/pages/loading.dart
+++ world_time/lib/pages/loading.dart
@@ -6,6 +6,28 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+
+  void getData() async {
+
+    // Simulate network request for username
+    String username = await Future.delayed(Duration(seconds: 3), () {
+      return 'Tiger';
+    });
+
+    // Simulate network request to get bio of the username.
+    String bio = await Future.delayed(Duration(seconds: 2,), () {
+      return 'Singer, coder & adventerous';
+    });
+
+    print('$username - $bio');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
